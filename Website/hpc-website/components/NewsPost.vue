5fe152fe8aac7da859b40ad57f6daf0b5082ce6e
Nicolas Plank <nicolas.plank@sz-ybbs.ac.at>
2021-02-26 10:30:33 +0100

    NewsPost CSS

--- Website/hpc-website/components/NewsPost.vue
+++ Website/hpc-website/components/NewsPost.vue
@@ -8,13 +8,17 @@
     <p class="news-date">{{blok.date}}</p>
     <img class="mx-auto py-8" :src="blok.img.filename">
     <p class="news-intro py-8 px-24">{{ blok.intro }}</p>
-    <article class="px-24 my-24 news-body justify white">
+    <article class="px-24 my-24 news-body justify white mx-auto">
       <rich-text-renderer
         :document="blok.body"
       />
     </article>
-    <iframe class="iframe" width="1120" :src="blok.youtube.cached_url" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <div v-if="blok.youtube.cached_url" class="">
+      <iframe class="iframe" width="1120" :src="blok.youtube.cached_url" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+    <div v-else class="">
 
+    </div>
   </div>
 </template>
 
