6b2418317e740b2c361c2711a5fe9b7a71efbb5d
nicolas.plank <nicolas.plank@sz-ybbs.ac.at>
2021-03-08 14:05:09 +0100

    Installed Storyblok-nuxt Connection (npm)

--- Website/hpc-website/components/HeaderTextImageButton.vue
+++ Website/hpc-website/components/HeaderTextImageButton.vue
@@ -2,21 +2,20 @@
   <div
     v-editable="blok"
     class=" white htib">
-    <div class="headertext text-center">
-      <span class="title text-center">{{ blok.name }}</span>
-    </div>
-
-    <div class="headertextDiv">
-      <div class="justify">
-        <rich-text-renderer
-          :document="blok.body"
-        />
+      <div class="headertext text-center">
+        <span class="title text-center">{{ blok.name }}</span>
       </div>
-      <img width="700px" height="auto" class="my-24 headertextImage" :src="transformImage(blok.img.filename, '700x0')"></img>
+      <div class="headertextDiv">
+        <div class="justify">
+          <rich-text-renderer
+            :document="blok.body"
+          />
+        </div>
+        <img width="700px" height="auto" class="my-24 headertextImage" :src="transformImage(blok.img.filename, '700x0')"></img>
+      </div>
+      <div class="text-center">
+        <a :href="blok.location.cached_url" class="welcomeButton mx-auto">{{blok.locationname}}</a>
     </div>
-    <div class="text-center">
-      <a :href="blok.location.cached_url" class="welcomeButton mx-auto">{{blok.locationname}}</a>
-  </div>
   </div>
 </template>
 
